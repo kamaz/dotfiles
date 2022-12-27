b70f127d0e99c304ad93bb1b8594e97629b1ac6a
Kamaz <kamaz@xsimple.org>
2022-12-27 22:04:02 +0000

    feat: add nvim and chezmoi config

---
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,115 @@
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+--Remap space as leader key
+-- keymap("", "<Space>", "<Nop>", opts)
+keymap("", ",", "<Nop>", opts)
+vim.g.mapleader = ","
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Clear highlights
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+
+-- Close buffers
+keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Plugins --
+
+-- NvimTree
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope
+-- keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+-- keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+-- keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+-- keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+local telescope_builtin = require('telescope.builtin')
+keymap('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [f]iles' })
+keymap('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [h]elp' })
+keymap('n', '<C-p>', telescope_builtin.git_files, { desc = '[S]earch' })
+keymap('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [w]ord' })
+keymap('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [g]rep' })
+keymap('n', '<leader>sG', telescope_builtin.live_grep, { desc = '[S]earch by [G]ist' })
+keymap('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [d]iagnostics' })
+keymap('n', '<leader>sp', ":Telescope projects<CR>", { desc = '[S]earch [p]rojects' })
+
+-- Git
+keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
+
+-- DAP
+keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
+keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
+keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
+keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", opts)
+keymap("n", "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", opts)
+keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
+keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
+keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
+keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+
+-- Lsp
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+
+
+-- Make file executable
+keymap("n", "<leader>x", "<cmd>!chmod +x%<CR>", opts)
+
+-- Keep the cursor at the beginning of line
+keymap("n", "J", "mzJ`z")
+
+-- Undotree
+keymap("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- Move lines in visual mode
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+-- When scrolling keep cursor in the middle
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+
+-- When searching keep search terms in the middle
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
